8edd23713580a2fff2dacfc17ba5481538bc2c4e
Jonathon Vandezande <jevandezande@gmail.com>
2015-05-18 14:25:01 -0400

    Rewrote using generalized coordinates.

--- programming/2/jevandezande/proj2.py
+++ programming/2/jevandezande/proj2.py
@@ -11,8 +11,8 @@ from itertools import combinations_with_replacement as cwr
 # Create a molecule
 mol = Molecule(open('../../1/extra-files/molecule.xyz').read(), 'Bohr')
 
-single_displace_form = 'disp/a{}_{}{}'
-double_displace_form = 'disp/a{}_{}{}.a{}_{}{}'
+single_displace_form = 'disp/q{}{}'
+double_displace_form = 'disp/q{}{}_q{}{}'
 
 input_form = """molecule {{
 {}
@@ -40,11 +40,22 @@ def write_input(name, molecule, theory='scf'):
     open(name, 'w').write(input_form.format(geom_str, theory))
 
 
-def displace(molecule, atom1, coord1, disp1, atom2, coord2, disp2):
+def displace(molecule, q1, disp1, q2, disp2):
     """
     Move the specified coordinate and atom the specified amount
+    Must convert from q to xyz
+    :param molecule: the molecule to be displaced
+    :param q1: coordinate 1
+    :param disp1: displacement for coordinate 1
+    :param q2: coordinate 2
+    :param disp2: displacement for coordinate 2
+    :return: a new Molecule with the specified displacements
     """
     copy = molecule.copy()
+    atom1 = int(q1 / 3)
+    coord1 = q1 % 3
+    atom2 = int(q2 / 3)
+    coord2 = q2 % 3
     copy.geom[atom1][coord1] += disp1
     copy.geom[atom2][coord2] += disp2
 
@@ -54,6 +65,7 @@ def displace(molecule, atom1, coord1, disp1, atom2, coord2, disp2):
 def run(name):
     """
     Run the specified input file
+    :param name: the name of the input file
     """
     # Name the oputput file the same, except for the extension
     out = '.'.join(name.split('.')[:-1]) + '.out'
@@ -62,93 +74,92 @@ def run(name):
 
 
 def get_energy(output_file):
+    """
+    Read the energy from a file, throw an exception if it cannot find the energy
+    :param output_file: the name of the file containing the energy
+    :return: the energy as a float
+    """
     lines = open(output_file).readlines()
     for line in reversed(lines):
         if line[:18] == '    Total Energy =':
             return float(line.split()[-1])
+    raise Exception('Cannot find the energy')
 
-def E1(atom, coord, sign):
-    output_file = single_displace_form.format(atom, coord, sign) + '.out'
+
+def E1(q1, sign):
+    """
+    Read a singly displaced energy
+    :param q1: the coordinate
+    :param sign: the sign of the displacement
+    :return: energy as a float
+    """
+    output_file = single_displace_form.format(q1, sign) + '.out'
     return get_energy(output_file)
 
-def E2(atom1, coord1, atom2, coord2, sign1, sign2):
-    if atom1 > atom2:
-        atom1, atom2 = atom2, atom1
-    if atom1 == atom2 and coord1 > coord2:
-        coord1, coord2 = coord2, coord1
 
-    output_file = double_displace_form.format(atom1, coord1, sign1,
-                                              atom2, coord2, sign2) + '.out'
+def E2(q1, sign1, q2, sign2):
+    """
+    Read a doubly displaced energy
+    :param q1: coordinate 1
+    :param sign1: the sign of displacement 1
+    :param q2: coordinate 2
+    :param sign2: the sign of displacement 2
+    :return: energy as a float
+    """
+    # Use symmetry
+    if q1 > q2:
+        q1, q2 = q2, q1
+
+    output_file = double_displace_form.format(q1, sign1, q2, sign2) + '.out'
     
     return get_energy(output_file)
 
 
 DISP = 0.005
-class Calc_Iter:
-    def __init__(self, mol):
-        self.mol = mol
-        
-    def __iter__(self):
-        # Iterate again over all possible unique combinations of atoms and their coordinates
-        # Allows for double counting of an atom
-        for (atom1, coord1), (atom2, coord2) in cwr(np.ndindex(self.mol.geom.shape), r=2):
-            print(atom1, coord1, "    ", atom2, coord2)
-            # Iterate over all ways to displace, don't displace the same atom coordinate twice
-            if atom1 == atom2 and coord1 == coord2:
-                yield atom1, coord1, atom2, coord2, DISP, 0
-                yield atom1, coord1, atom2, coord2, -DISP, 0
-            else:
-                yield atom1, coord1, atom2, coord2, DISP, DISP
-                yield atom1, coord1, atom2, coord2, -DISP, -DISP
-
-
 os.makedirs('disp/', exist_ok=True)
 
-#mol = Molecule('1\n\nH 0 0 0')
 
+# Run the reference geometry
 e0_in = 'disp/e0.in'
 write_input(e0_in, mol)
 run(e0_in)
-lines = open('disp/e0.out').readlines()
-for line in reversed(lines):
-    if line[:18] == '    Total Energy =':
-        e0 = float(line.split()[-1])
+e0 = get_energy('disp/e0.out')
 print("E0: " + str(e0))
 
-# Iterate over all atoms and their coordinates
-for atom1, coord1 in np.ndindex(mol.geom.shape):
-    # Iterate again over all atoms and their coordinates
-    my_iter = Calc_Iter(mol)
-    for atom1, coord1, atom2, coord2, disp1, disp2 in my_iter:
-        sign1 = '+' if disp1 > 0 else '-'
-        if disp2 == 0:
-            inp_name = single_displace_form.format(atom1, coord1, sign1) + '.in'
-        else:
-            sign2 = '+' if disp2 > 0 else '-'
-            inp_name = double_displace_form.format(atom1, coord1, sign1, atom2, coord2, sign2) + '.in'
-        displaced = displace(mol, atom1, coord1, disp1, atom2, coord2, disp2)
-        write_input(inp_name, displaced)
-        run(inp_name)
+# Iterate over all pairs of coordinates in the upper triangle
+#for q1, q2 in cwr(range(3*len(mol)), r=2):
+#    # Displace forwards and back
+#    for disp in [DISP, -DISP]:
+#        sign = '+' if disp > 0 else '-'
+#        if q1 == q2:
+#            print(q1)
+#            inp_name = single_displace_form.format(q1, sign) + '.in'
+#            displaced = displace(mol, q1, disp, q2, 0)
+#        else:
+#            print(q1, q2)
+#            inp_name = double_displace_form.format(q1, sign, q2, sign) + '.in'
+#            displaced = displace(mol, q1, disp, q2, disp)
+#        write_input(inp_name, displaced)
+#        run(inp_name)
 
 
+# Form an empty Hessian matrix
 H = np.zeros((3*len(mol), 3*len(mol)))
 
-for atom1, coord1 in np.ndindex(mol.geom.shape):
-    for atom2, coord2 in np.ndindex(mol.geom.shape):
-        if atom1 == atom2 and coord1 == coord2:
-            print(atom1, coord1)
-            H[3*atom1 + coord1][3*atom2 + coord2] = \
-                (E1(atom1, coord1, '+') + E1(atom2, coord2, '-') - e0)/(DISP**2)
-        else:
-            print(atom1, coord1, atom2, coord2)
-            H[3*atom1 + coord1][3*atom2 + coord2] = \
-                (E2(atom1, coord1, atom2, coord2, '+', '+')
-                + E2(atom1, coord1, atom2, coord2, '-', '-')
-                - E1(atom1, coord1, '+') - E1(atom1, coord1, '-')
-                - E1(atom2, coord2, '+') - E1(atom2, coord2, '-')
-                + 2*e0) / (2*DISP**2)
+# Iterate over all coordinates in the hessian
+for q1, q2 in np.ndindex(H.shape):
+    # Diagonal elements
+    if q1 == q2:
+        H[q1][q2] = (E1(q1, '+') + E1(q1, '-') - 2*e0)/(DISP**2)
+    # Off-diagonal elements
+    else:
+        H[q1][q2] = (E2(q1, '+', q2, '+') + E2(q1, '-', q2, '-')
+            - E1(q1, '+') - E1(q1, '-') - E1(q2, '+') - E1(q2, '-')
+            + 2*e0) / (2*DISP**2)
 
 
-hess_form = '{: 5.3f} '*len(mol)*3
+hess_form = '{: 15.13f} '*len(mol)*3
 out = '\n'.join([hess_form.format(*row) for row in H])
 print(out)
+
+open('hessian.dat', 'w').write(out)
