b2da998f7a38c2eba7469d46161da031f3fdbfb0
adabbott <adabbott@iastate.edu>
2015-06-18 16:52:46 -0400

    6/18/2015 Started Project

---
+++ 1/Adam/project1.py
@@ -0,0 +1,12 @@
+import sys
+sys.path.insert(0, '../extra-files')
+sys.path.insert(0, '../../0/Adam')
+
+from molecule import Molecule
+
+mol = Molecule(open("../extra-files/molecule.xyz").read())
+
+
+
+
+
