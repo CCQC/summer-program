0f0c88bcacb5d4b594ccfb48bf085187d89c0f6d
adabbott <adabbott@iastate.edu>
2015-06-22 14:58:50 -0400

    start of project 1

--- 1/Adam/project1.py
+++ 1/Adam/project1.py
@@ -6,6 +6,15 @@ from molecule import Molecule
 
 mol = Molecule(open("../extra-files/molecule.xyz").read())
 
+mol = mol.bohr_to_ang()
+#read Hessian Values
+hessian_values = open('../extra-files/hessian.dat').readlines()
+print (hessian_values)
+#collect hessain values into matrix
+for row in hessian_values:
+    row.split()
+
+
 
 
 
