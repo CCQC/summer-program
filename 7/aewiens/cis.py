5dfb2251348da9f04a2b043a699fd3371bf01abd
aewiens <aewiens@uga.edu>
2015-07-17 15:15:21 -0400

    cis finished

--- 7/aewiens/cis.py
+++ 7/aewiens/cis.py
@@ -1,46 +1,68 @@
 import numpy as np
-from scipy import linalg as la
+
+from psi4_helper import get_nocc, get_nbf
 
 import sys
 sys.path.insert(0,"../../5/aewiens")
-from uhf import UHF
+#sys.path.insert(0,"../../5/avcopan")
 
-from psi4_helper import get_nocc, get_nbf, get_maxiter
+from uhf import UHF
 
 class CIS:
 
     def __init__(self,mol,mints):
+
         self.nbf = get_nbf(mints)
-        self.norb = 2*self.nbf
         self.nocc = get_nocc(mol)
+
+        uhf = UHF(mol,mints)
+        uhf.get_energy()
+        self.E0 = uhf.E
+        self.e = uhf.e
+
+        self.Gmo = transform_integrals(uhf.g, uhf.C)
+
+        self.norb = 2*self.nbf
         self.nvirtual = self.norb - self.nocc
-        self.maxiter = get_maxiter
-        self.uhf = UHF(mol,mints)
-        self.E_uhf = self.uhf.get_energy()
-        self.e = self.uhf.e
-        self.C = self.uhf.C
-        self.g = self.uhf.g
+        self.ndet = self.nvirtual*self.nocc
 
-    def cis_energy(self):
 
+    def singles_iterator(self):
+        """
+        return a list of all (i,a) single excitations  xi_i -> xi_a
+        """
+        return [(i,a) for i in range(self.nocc) for a in range(self.nocc,self.nocc+self.nvirtual)]
+
+    def cis_energies(self):
+        """
+        Print out a list of CIS excited states and their energies (Eh)
+        Return an ordered list of CIS excitation energies
+        """
         ##  rename object variables 
-        norb, nocc, nvirtual, e = self.norb, self.nocc, self.nvirtual, self.e
+        ndet, E0, e, Gmo = self.ndet, self.E0, self.e, self.Gmo
 
-        Gmo = self.transform_integrals()
-        Gmo = Gmo - Gmo.swapaxes(2,3)         ### antisymmetrize
+        excite = self.singles_iterator()
 
-        tH = np.zeros((nocc*nvirtual,nocc*nvirtual))
-        for i in range(nocc):
-            for a in range(nocc,nvirtual):
-                for j in range(nocc):
-                    for b in range(nocc,nvirtual):
-                        tH[j*nvirtual+a,i*nvirtual+b] = (e[a] - e[i])*np.kron(a,b)*np.kron(i,j) + Gmo[a,j,i,b]
-        e_cis, L = np.linalg.eigh(tH)
-        return e_cis
+        ##  initialize CIS hamiltonian tH
+        tH = np.zeros((ndet,ndet))
 
-    def transform_integrals(self):
-        g,C = self.g, self.C
-        return np.einsum("Pp,Pqrs->pqrs",C,
-                    np.einsum("Qq,PQrs->Pqrs",C,
-                        np.einsum("Rr,PQRs->PQrs",C,
-                            np.einsum("Ss,PQRS->PQRs",C,g))))
+        ## build CIS Hamiltonian tH
+        for P, (i,a) in enumerate(excite):
+            for Q, (j,b) in enumerate(excite):
+                tH[P,Q] += Gmo[a,j,i,b] + (e[a] - e[i])*(a==b)*(i==j) 
+                
+        ##  diagonalize tH
+        E, C = np.linalg.eigh(tH)
+
+        print( "{:>6s}{:>15s}".format("State","Energy") )
+        for i, en in enumerate(E):
+            print("{:6d}  {: >16.11f}".format(i,en) )
+
+        self.E = E
+        return E
+
+def transform_integrals(g,C):
+  return np.einsum('Pp,Pqrs->pqrs', C,
+           np.einsum('Qq,PQrs->Pqrs', C,
+             np.einsum('Rr,PQRs->PQrs', C,
+               np.einsum('Ss,PQRS->PQRs', C, g))))
