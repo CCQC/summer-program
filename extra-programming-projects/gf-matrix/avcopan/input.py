a3bdfb4314c61028e766483430f7890c91357316
Andreas Copan <avcopan@gmail.com>
2016-05-20 18:42:01 -0400

    Edited comments in GF matrix code

--- extra-programming-projects/gf-matrix/avcopan/input.py
+++ extra-programming-projects/gf-matrix/avcopan/input.py
@@ -1,4 +1,4 @@
-# Problem 5.  This is the main program, which completes each part of the assignment using
+# This is the main program, which completes each part of the assignment using
 # the modules "molecule" and "vib".
 import numpy as np
 from vib import IntCoVibAnalysis
@@ -47,7 +47,7 @@ U = np.array(
    [     0,     0,    +n,     0,    -n,     0]] # s6
 )
 
-# (a). compute the G matrices for HOOH and DOOD
+# compute the G matrices for HOOH and DOOD
 
 G = hooh.compute_g_matrix() # compute G matrix in unsymmetrized internal coordinate basis
 m = hooh.masses
@@ -65,45 +65,12 @@ vib_dood = IntCoVibAnalysis(F, G, blockdims=(4, 2))
 print("DOOD G matrix")
 print G.round(14)
 
-# (b). perform normal mode analysis and compute the harmonic frequencies
+# perform normal mode analysis and compute the harmonic frequencies
 
 vib_hooh.print_cm_frequencies()
 vib_dood.print_cm_frequencies()
 
-# (c). compute the total energy distributions of the normal modes
+# compute the total energy distributions of the normal modes
 
 vib_hooh.print_total_energy_distribution()
 vib_dood.print_total_energy_distribution()
-
-# (d). numerically demonstrate the Teller-Redlich product rule -- prints out the left-hand
-#      and right-hand sides first for the unfactored and then for the factored form
-hI = hooh.moments
-dI = dood.moments
-hm = hooh.masses
-dm = dood.masses
-hM = sum(hm)
-dM = sum(dm)
-hv = vib_hooh.frequencies
-dv = vib_dood.frequencies
-n  = vib_hooh.blockdims
-slices = vib_hooh.slices
-p = (1., 2.)
-q = [(0., 1.), (0., 1.), (1., 0.)]
-r = (3., 3.)
-
-# unfactored
-hooh.diagonalize_inertia_tensor()
-lhs = np.prod(dv)/np.prod(hv)
-rhs = np.sqrt(dM/hM)**3 * np.sqrt( np.prod(dI)/np.prod(hI) ) * np.sqrt( np.prod(hm)/np.prod(dm) )**3
-print("\nUnfactored Teller-Redlich")
-print lhs
-print rhs
-
-for g, slc in enumerate(slices):
-  print("Factored Teller-Redlich, Irrep {:d}".format(g))
-  lhs = np.prod(dv[slc])/np.prod(hv[slc])
-  rhs = np.sqrt(dM/hM)**p[g] \
-      * np.sqrt(np.prod([ (dI[x]/hI[x])**q[x][g] for x in range(3)     ])) \
-      * np.sqrt(hm[0]/dm[0])**r[g]
-  print lhs
-  print rhs
