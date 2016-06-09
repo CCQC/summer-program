1e06b6a781f756cf3919efdfa74793ee7850d7c4
Morgan James <morganjames@s172-21-69-h64.paws.uga.edu>
2016-06-09 11:05:28 -0400

    started project 0

--- 0/mjames84/molecule.py
+++ 0/mjames84/molecule.py
@@ -1,4 +1,18 @@
+import numpy as np
 
+class Molecule(object):
+    """
+    A simple molecule
+    """
 
-Class Molecule:
-    pass
+    def __init__(self, geom_str, units = "Angstrom"):
+        self.read(geom_string)
+        self.units = units
+
+    def read(self, geom_string):
+        lines = geom_str.split("\n")
+        num = int(lines[0])
+        atoms = []
+        coords = []
+        for line in lines: 
+     
