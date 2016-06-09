e6dec6aebcb7bbcb95940f19f9f337bba8942241
Sean Fitzpatrick <sean@s172-21-230-h133.paws.uga.edu>
2016-06-09 17:00:52 -0400

    messing with file IO

--- 0/huples-cat/molecule.py
+++ 0/huples-cat/molecule.py
@@ -1,4 +1,21 @@
+import numpy as np
+import sys
+sys.path.insert(0, '../../extra_files')
+import masses as atomlib                   ##contains get_mass('x') and get_charge('x') methods
 
-
-Class Molecule:
-    pass
+Class Molecule(object):
+    def __init__:
+        f = open('molecule.xyz', r)        ##file object molecule.xyz is f
+	f.seek(-1)
+        file_length = f.tell()
+	f.seek(0)
+	while(f.tell() < file_length):
+	    input = ''
+            while(f.read(1) != '\n'):      ##cat byte by byte to input string until '\n'
+	        f.seek(-1, 1)
+                input += f.read(1)
+	    if (input.length == 0):        ##if blank move on
+	    elif (input.length == 1):      ##if one byte try for natom
+	        try:
+	            int(input)
+		except ValueError:
