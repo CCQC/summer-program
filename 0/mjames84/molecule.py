4f83ebb977feda3dddbb7fb20cb39ffacc72b4ed
Morgan James <morganjames@Morgans-MacBook-Air.local>
2016-06-09 18:07:01 -0400

    working on project 0

--- 0/mjames84/molecule.py
+++ 0/mjames84/molecule.py
@@ -10,9 +10,28 @@ class Molecule(object):
         self.units = units
 
     def read(self, geom_string):
-        lines = geom_str.split("\n")
-        num = int(lines[0])
         atoms = []
-        coords = []
-        for line in lines: 
-     
+        geom = []
+        lines = geom_str.split("\n")
+        for item in lines[2:]:
+            atom, x, y, z = item.split()
+            atoms.append(atom)
+            geom.append([float(x),float(y),float(z)]) 
+        self.atoms = atoms
+        self.geom = np.array(geom)
+
+    def __len__(self):
+        return len(atoms)
+
+    def to_bohr(self):
+        if self.units == "Angstrom":
+            self.units = "Bohr"
+            self.geom *= 1.889725989
+
+    def to_angstrom(self):
+        if self.units == "Bohr":
+            self.units = "Angstrom"
+            self.geom /= 1.889725989
+
+    
+    
