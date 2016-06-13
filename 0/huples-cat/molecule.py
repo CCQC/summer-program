caad2e6593bb7d10c1ccc325b27e6e7fbce7b7df
Sean Fitzpatrick <sean@s172-21-198-h102.paws.uga.edu>
2016-06-13 14:31:45 -0400

    Finished init

--- 0/huples-cat/molecule.py
+++ 0/huples-cat/molecule.py
@@ -4,18 +4,35 @@ sys.path.insert(0, '../../extra_files')
 import masses as atomlib                   ##contains get_mass('x') and get_charge('x') methods
 
 Class Molecule(object):
-    def __init__:
-        f = open('molecule.xyz', r)        ##file object molecule.xyz is f
-	f.seek(-1)
-        file_length = f.tell()
-	f.seek(0)
-	while(f.tell() < file_length):
-	    input = ''
-            while(f.read(1) != '\n'):      ##cat byte by byte to input string until '\n'
-	        f.seek(-1, 1)
-                input += f.read(1)
-	    if (input.length == 0):        ##if blank move on
-	    elif (input.length == 1):      ##if one byte try for natom
-	        try:
-	            int(input)
-		except ValueError:
+    def __init__(self):
+        f = open('molecule.xyz', r)        ##file object for molecule.xyz is f
+        for line in f:                     ##iterate over lines
+            input = line.split()           ##returns array of strings
+            if(len(input) == 1):           ##if input array has only one element then is either:
+                try:
+                    self.natom = int(input[0])                                ##an int natom
+                except ValueError:
+                    if(input[0] == 'Angstrom' or input[0] == 'Bohr'):         ##or string units
+                        self.units = input[0]
+                    else:
+                        print('Format error')                                 ##or neither? then error and exit
+                        exit(0)
+            elif(len(input) == 4):         ##if input array has four elements then process xyz:
+                try:
+                    self.charges[] += atomlib.get_charge(input[0])            ##is atomic symbol recognized in masses.py?
+                    self.masses[] += atomlib.get_mass(input[0])               ##then assign charge and mass
+                except KeyError:
+                    print('Unrecognized atomic symbol')
+                    exit(0)
+                self.labels[] += input[0]                                     ##assign atomic symbol
+                for i in range(1, 3):
+                    try:
+                        self.xyz[].append(float(input[i]))                    ##check and assign coords
+                    except ValueError:
+                        print('Format error in coordinate for %s'), input[0]
+            else:                          ##if input array has unrecognized amount of elements, then error and exit
+                print('Format error')
+                exit(0)
+        self.geom = np.matrix(self.xyz)                            ##make numpy array
+        self.geom = np.reshape(self.geom, (3, self.natom))         ##reformat geom matrix to be 3 by number of atoms
+        f.close()
