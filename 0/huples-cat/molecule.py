4fd164b5a4d8a0fde3be38bb8263a43959853c94
Sean Fitzpatrick <sean@s172-21-198-h102.paws.uga.edu>
2016-06-13 15:29:03 -0400

    working on __str__

--- 0/huples-cat/molecule.py
+++ 0/huples-cat/molecule.py
@@ -1,38 +1,39 @@
 import numpy as np
 import sys
-sys.path.insert(0, '../../extra_files')
-import masses as atomlib                   ##contains get_mass('x') and get_charge('x') methods
+sys.path.insert(0,'../../extra-files/')
+from masses import get_mass, get_charge
 
-Class Molecule(object):
+class Molecule():
     def __init__(self):
-        f = open('molecule.xyz', r)        ##file object for molecule.xyz is f
-        for line in f:                     ##iterate over lines
-            input = line.split()           ##returns array of strings
-            if(len(input) == 1):           ##if input array has only one element then is either:
-                try:
-                    self.natom = int(input[0])                                ##an int natom
-                except ValueError:
-                    if(input[0] == 'Angstrom' or input[0] == 'Bohr'):         ##or string units
-                        self.units = input[0]
-                    else:
-                        print('Format error')                                 ##or neither? then error and exit
-                        exit(0)
-            elif(len(input) == 4):         ##if input array has four elements then process xyz:
-                try:
-                    self.charges[] += atomlib.get_charge(input[0])            ##is atomic symbol recognized in masses.py?
-                    self.masses[] += atomlib.get_mass(input[0])               ##then assign charge and mass
-                except KeyError:
-                    print('Unrecognized atomic symbol')
-                    exit(0)
-                self.labels[] += input[0]                                     ##assign atomic symbol
-                for i in range(1, 3):
-                    try:
-                        self.xyz[].append(float(input[i]))                    ##check and assign coords
-                    except ValueError:
-                        print('Format error in coordinate for %s'), input[0]
-            else:                          ##if input array has unrecognized amount of elements, then error and exit
-                print('Format error')
-                exit(0)
-        self.geom = np.matrix(self.xyz)                            ##make numpy array
-        self.geom = np.reshape(self.geom, (3, self.natom))         ##reformat geom matrix to be 3 by number of atoms
+        f = open('../../extra-files/molecule.xyz')
+        lines = f.readlines()
         f.close()
+        self.natom = int(lines[0])
+        self.units = lines[1]
+        # initialize stuff
+        self.labels = []
+        self.charges = []
+        self.masses = []
+        self.xyz = []
+        # get geoms
+        for line in lines[2:]:
+            atom, x, y, z = line.split()
+            self.labels.append(atom)
+            self.charges.append(get_charge(atom))
+            self.masses.append(get_mass(atom))
+            self.xyz.append((float(x), float(y), float(z)))
+        self.geom = np.array(self.xyz)                            # make numpy array
+
+    def __str__(self):
+        """
+        Convert into an xyz formatted string
+        """
+        my_str = "%i \n" % self.natom
+        my_str += "%s \n" % self.units
+        for i in range(0, self.natom - 1):
+            my_str += "%s\t%f\t%f\t%f\n" % (self.labels[i], self.xyz[i][0], self.xyz[i][1], self.xyz[i][2])
+        return my_str
+
+if __name__ == "__main__":
+    mol = Molecule()
+    print(mol)
