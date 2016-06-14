21db31f087350fdefa8e0af9f403e26d5a0a158c
Sean Fitzpatrick <sean@s172-21-230-h133.paws.uga.edu>
2016-06-14 15:32:53 -0400

    start p1

--- 1/huples-cat/hessian.py
+++ 1/huples-cat/hessian.py
@@ -1,5 +1,12 @@
 import sys
-sys.path.insert(0, '../extra-files/')
+sys.path.insert(0, '../../extra-files/')
 sys.path.insert(0, '../../0/huples-cat/')
 from masses import get_charge, get_mass
-import molecule
+from molecule import Molecule
+
+#open Hessian
+lines = open('../../extra-files/hessian.dat').readlines()
+
+#create molecule object
+mol = Molecule('../../extra-files/molecule.txt')
+
