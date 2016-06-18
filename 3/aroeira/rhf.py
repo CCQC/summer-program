3474b759730857abd2cbc2b876cd53651be0f097
Gustavo Aroeira <aroeira@uga.edu>
2016-06-18 18:42:14 -0400

    Proj 3 finished

--- 3/aroeira/rhf.py
+++ 3/aroeira/rhf.py
@@ -35,44 +35,45 @@ class RHF:
 
         self.maxiter = psi4.get_global_option('MAXITER')
         self.e_convergence = psi4.get_global_option('E_CONVERGENCE')
-
         self.nbf = mints.basisset().nbf()
+        self.vu = np.matrix(np.zeros((self.nbf, self.nbf)))
+    
+    def build_vu(self, D):
+        """
+        Construct a v matrix using the density matrix and the four index integrals
+        """
+
+        r = range(int(self.nbf))
+        self.vu = np.matrix(np.zeros((self.nbf, self.nbf)))
+        for u in r:
+            for v in r:
+                for p in r:
+                    for q in r:
+                        self.vu[u, v] += (2 * self.g[u, v, p, q] - self.g[u, p, v, q]) * D[q, p] 
 
     def compute_energy(self):
         """
         Compute the rhf energy
         :return: energy
         """
-        l = int(self.nbf)
-        r = range(l)
         X = np.matrix(la.inv(la.sqrtm(self.S)))
-        D = np.matrix(np.zeros((l, l)))
-        vu = np.matrix(np.zeros((l, l)))
+        D = np.matrix(np.zeros((self.nbf, self.nbf)))
         h = self.T + self.V
         E0 = 0
         for count in range(self.maxiter):
-            F = h + vu
+            F = h + self.vu
             Ft = X * F * X
             e, Ct = la.eigh(Ft)
             C = X * np.matrix(Ct)
             DOC = np.matrix(C[:,:self.ndocc])
             D = DOC*DOC.T
-            vu = np.matrix(np.zeros((l, l)))
-            for u in r:
-                for v in r:
-                    for p in r:
-                        for q in r:
-                            vu[u, v] += (2 * self.g[u, v, p, q] - self.g[u, p, v, q]) * D[q, p] 
-            E1 = np.sum((2 * np.array(h) + np.array(vu))*np.array(D.T)) + self.V_nuc
-            print '{:<5.10f}    {:>5.10f}'.format(E1, E1-E0)
+            self.build_vu(D)
+            E1 = np.sum((2 * np.array(h) + np.array(self.vu))*np.array(D.T)) + self.V_nuc
+            psi4.print_out('Iteration {:<d}   {:.10f}    {:.10f}\n'.format(count, E1, E1-E0))
             if abs(E1 - E0) < self.e_convergence:
+                psi4.print_out('\nFinal Energy: {:<5.10f}'.format(E1))
                 break
             else:
                 E0 = E1
         else:
-            print(':(   Does not converge   :(')
-         
-        
-
-    
-        
+            psi4.print_out('\n:(   Does not converge   :(')
