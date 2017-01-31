a436077dbc6129a0a10051941c3d90393f84869b
Boyi Zhang <bz11252@uga.edu>
2017-01-31 13:07:58 -0500

    define class variables

--- 8/bz3wp/ccd.py
+++ 8/bz3wp/ccd.py
@@ -1,4 +1,3 @@
-# Configuration Interaction Singles class
 
 import psi4
 import numpy as np
@@ -7,7 +6,7 @@ import scipy.linalg as la
 sys.path.insert(0, '../../5/bz3wp')
 from uhf import UHF
 
-class CIS:
+class CCD:
         
     def __init__(self, uhf):                             # getting class variables from UHF
         self.e = uhf.e                                   # UHF orbital energies
@@ -18,39 +17,9 @@ class CIS:
         self.ndet = self.nocc*self.vir
         self.C = uhf.C                                   # MO coefficients
     
-    def transform_integrals(self, C, g_ao):              # integral transformation function
-        return np.einsum('pqrs, pP, qQ, rR, sS-> PQRS',g_ao, C, C, C, C)
-         
-    def single_excite(self):                             # all possible single excitations
-        return[(i,a) for i in range(self.nocc) for a in range(self.nocc, self.nto)]
- 
-    def get_energy(self):
-        
-        ndet,E0, e, C = self.ndet, self.E0, self.e, self.C
-        
-        g_mo = self.transform_integrals(C, uhf.g)        # call integral transformation function
-        
-        Hc = np.zeros((ndet, ndet))                      # Hcorrelation = Hcore - E0
-
-        for P, (i,a) in enumerate(self.single_excite()): # calculating matrix elements of single excitations
-            for Q, (j,b) in enumerate(self.single_excite()):
-                Hc[P,Q] = g_mo[a,j,i,b] + (e[a]-e[i])*(a==b)*(i==j)
-
-        E, C = np.linalg.eigh(Hc)                        # diagonalize correlation hamiltonian
-
-        
-        print('{:>6s}{:>10s}'.format('State', 'Energy'))
-        for i, ei in enumerate(E): 
-            print('{:6d} {:>16.11f}'.format(i,ei))
-        
-        self.E = E                                       # saving value
-        
-        return E
     
 # testing
 if __name__=='__main__':
 
     uhf = UHF('../../5/bz3wp/Options.ini')
     uhf.get_energy()
-    cis = CIS(uhf)
-    cis.get_energy()
