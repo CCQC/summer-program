af908ce5ea074b26c96f431ec933217153fa48d3
Avery Wiens <aewiens@uga.edu>
2017-02-06 19:41:56 -0500

    some new tricks i've learned

--- 3/aewiens/rhf.py
+++ 3/aewiens/rhf.py
@@ -1,61 +1,102 @@
-import numpy as np
-from scipy import linalg as la
-from psi4_helper import get_docc, get_nbf, get_conv, get_maxiter
+#!/usr/bin/env python3
 
-class RHF:
-# Class for obtaining the HF energy of a closed shell system
+import psi4, configparser, numpy as np
 
-    def __init__(self,mol,mints):
-        """
-        initialize rhf class
-        takes objects:
-        1. mol: a psi4 molecule, get_active_molecule()
-        2. mints: molecular integrals from libmints in psi4
-        """
-        self.docc = get_docc(mol)
-        self.nbf = get_nbf(mints)
-        self.conv = get_conv()
-        self.maxiter = get_maxiter()
+class RHF(object):
 
-        S = np.matrix(mints.ao_overlap() )
-        self.X = np.matrix(la.funm(S,lambda x : x**(-0.5)))
+	def __init__(self,molecule,mints):
 
-        T = np.matrix(mints.ao_kinetic() )
-        V = np.matrix(mints.ao_potential() )
-        self.H = T+V
-        self.Vnu = mol.nuclear_repulsion_energy()
+		self.docc = self.getDocc(molecule)       
+		self.Vnu  = molecule.nuclear_repulsion_energy()
+		self.E    = 0
 
-        self.G = np.array(mints.ao_eri() ).transpose((0,2,1,3))
-        self.D = np.zeros((self.nbf,self.nbf))
-        self.E = 0
+		self.getIntegrals(mints)
+		self.D = np.zeros_like( self.S )
 
-    def get_energy(self):
-        """
-        print E and dE from each iteration
-        return rhf energy
-        """
+		self.converged = False
 
-        docc, nbf, conv, maxiter = self.docc, self.nbf, self.conv, self.maxiter
-        X, H, G, Vnu, D, E = self.X, self.H, self.G, self.Vnu, self.D, self.E
 
-        for i in range(maxiter):
-            J= np.einsum("ikjl,kl",G,D)
-            K = np.einsum("iklj,kl",G,D)
-            F = H+J-0.5*K                           ##  build fock matrix
-            tF = X*F*X                              ##  diagonalize fock matrix
-            e, tC = np.linalg.eigh(tF)              ##  eigenvalues & eigenvectors of fock matrix
-            C = X*tC                                ##  back-transform the eigenvectors
-            oC = C[:,:docc]                         ##  save occupied block of C matrix
-            D = 2*oC*oC.T                           ##  build density matrix
+	def getDocc(self,mol):
+		char = mol.molecular_charge()
+		nelec = -char
+		for A in range(mol.natom()):
+			nelec += mol.Z(A)
+		return int(nelec//2)
 
-            E0 = E
-            E = np.trace(0.5*(H+F)*D)+Vnu
-            dE = np.fabs(E-E0)
 
-            print( "{0:2d}{0:20.12f}{1:20.12f}".format(i,E,dE) )
-            if dE < self.conv : break
+	def getIntegrals(self,mints):
 
-            #### save object variables that changed during this iteration
-            self.E, self.e, self.C, self.D = E, e, C, D
+		self.V = np.array( mints.ao_potential() )
+		self.T = np.array( mints.ao_kinetic() )
+		self.G = np.array( mints.ao_eri() ).transpose((0,2,1,3))
 
-        return self.E
+		S = mints.ao_overlap()
+		S.power(-0.5, 1.e-16)
+		self.X = S.to_array() 
+		self.S = np.array( mints.ao_overlap() )
+
+
+	def computeEnergy(self):
+
+		H  = self.T + self.V
+		G  = self.G
+		X  = self.X
+		S  = self.S
+		D  = self.D
+		docc = self.docc
+
+		for i in range(100):
+
+			J= np.einsum("ikjl,kl",G,D)
+			K = np.einsum("iklj,kl",G,D)
+			F = H+2*J-K
+
+			tF = X.dot(F.dot(X))
+
+			e, tC = np.linalg.eigh(tF)
+
+			C    = X.dot(tC)
+			Cocc = C[:,:docc]
+			D    = Cocc.dot(Cocc.T)
+
+		
+			E0 = self.E
+			E  = np.trace((H+F) @ D)+ self.Vnu
+			dE = np.fabs(E-E0)
+
+			if __name__ == '__main__':
+				print("RHF  {:>4} {: >21.11}  {: >21.11}".format(i,E,dE))
+
+			if dE < 1e-12:
+				self.converged = True
+				self.writeOutput()
+				break
+
+			self.D = D
+			self.E = E
+
+		return E
+
+
+	def writeOutput(self):
+
+		if __name__ == '__main__':
+
+			if self.converged:
+				print("\nRHF procedure has converged.\nFinal RHF energy:{:20.11f}".format(self.E) )
+			if not self.converged:
+				print("RHF procedure did not converge. Sorry")
+
+
+
+
+if __name__ == '__main__':
+
+	config = configparser.ConfigParser()
+	config.read('Options.ini')
+	molecule   = psi4.geometry( config['DEFAULT']['molecule'] )
+	molecule.update_geometry()
+	basis = psi4.core.BasisSet.build(molecule, "BASIS", config['DEFAULT']['basis'],puream=0)
+	mints = psi4.core.MintsHelper(basis)
+	rhf   = RHF(molecule,mints)
+	rhf.computeEnergy()
