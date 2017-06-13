beb0f4f4f5ae51712211d64d1e23f878b8d92664
zachglick <glickzachary@gmail.com>
2017-06-13 15:17:33 -0400

    outlined molecule file

---
+++ 0/zachglick/molecule.py
@@ -0,0 +1,36 @@
+class Molecule :
+
+    # Default Methods
+
+    def __init__(self) :
+        pass
+
+    def to_bohr(self) :
+        pass
+
+    def to_angstrom(self) :
+        pass
+
+    def xyz_string(self) :
+        pass
+
+    def copy(self) :
+        pass
+
+    # Advanced Methods
+
+    def __len__(self) :
+        pass
+
+    def __str__(self) :
+        pass
+
+    def __repr__(self) :
+        pass
+
+    def __iter__(self) :
+        pass
+
+    def __add__(self, mol) :
+        pass
+
