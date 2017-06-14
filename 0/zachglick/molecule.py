bf9afd4868d6b8414137828e6c2813843c47c92f
zachglick <glickzachary@gmail.com>
2017-06-14 12:31:42 -0400

    Implemented iterator, added corresponding unit test

--- 0/zachglick/molecule.py
+++ 0/zachglick/molecule.py
@@ -1,11 +1,16 @@
 import masses as m
+from numpy import array
 
 class Molecule :
-
+    """
+    Represenation of a molecule
+    """
     # Default Methods
 
     def __init__(self, data) :
-
+        """
+        data -- contents of an .xyz file as a str
+        """
         data = data.split('\n')
         self.natom = int(data[0])
         self.units = data[1]
@@ -29,16 +34,25 @@ class Molecule :
         self.geom = [[float(coord) for coord in atom] for atom in self.geom]
         
     def to_bohr(self) :
+        """
+        Converts geometry to units of Bohrs
+        """
         if self.units == 'Angstrom' :
             self.units = 'Bohr'
             self.geom =[[coord * 1.889725989 for coord in atom] for atom in self.geom]
 
     def to_angstrom(self) :
+        """
+        Converts geometry to units of Angstroms
+        """
         if self.units == 'Bohr' :
             self.units = 'Angstrom'
             self.geom =[[coord / 1.889725989 for coord in atom] for atom in self.geom]
 
     def xyz_string(self) :
+        """
+        Returns (str) a representation of the molecular geometry in .xyz format
+        """
         coordinates = ["\t".join(str(coord) for coord in atom) for atom in self.geom]
         atoms = [zipped[0] + "\t" + zipped[1]
                  for zipped in zip(self.labels, coordinates)]
@@ -47,14 +61,23 @@ class Molecule :
 
 
     def copy(self) :
+        """
+        Returns (Molecule) a new molecule identical to this one
+        """
         return Molecule(self.xyz_string())
 
     # Advanced Methods
 
     def __len__(self) :
+        """
+        Returns (int) the number of atoms in the molecule
+        """
         return self.natom
 
     def __str__(self) :
+        """
+        Returns (str) a nice represenation of the molecule
+        """
         coordinates = ["(" +
                        ", ".join(str(coord) for coord in atom) +
                        ")"
@@ -65,12 +88,38 @@ class Molecule :
         return summary + "\n" + "\n".join(atoms)
     
     def __repr__(self) :
+        """
+        Returns (str) a representation of the molecular geometry in .xyz format
+        """
         return self.xyz_string()
 
     def __iter__(self) :
-        pass
+        """
+        Returns (iter) an iterator over the atoms of molecule
+        Each atom is formatted as a tuple: (label, array of coordinates)
+        """
+        self.iteration_index = 0
+        return self
+
+    def __next__(self) :
+        """
+        Returns (tuple) an atom and increments the iteration over atoms of the molecule.
+        Each atom is formatted as a tuple: (label, array of coordinates)
+        """
+        if(self.iteration_index < self.natom) :
+            ans = (self.labels[self.iteration_index], array(self.geom[self.iteration_index]))
+            self.iteration_index += 1
+            return ans
+        else :
+            raise StopIteration
 
     def __add__(self, mol) :
+        """
+        mol -- another object of the class molecule
+        
+        Adds mol to this object, increasing the number of atoms
+        """
+        
         if self.units == 'Angstroms' :
             mol.to_bohr()
         else :
--- 0/zachglick/test.py
+++ 0/zachglick/test.py
@@ -1,12 +1,16 @@
 import unittest
+import numpy
 from molecule import Molecule
 
 class Test(unittest.TestCase):
-    """ Tests for Molecule class in 'molecule.py' """
+    """ 
+    Tests for Molecule class in 'molecule.py'
+    """
     
     def test_self_variables(self):
-        """ Are the self variables (units, natom, masses, charges, geom)
-            from the xyz file initialized correctly? """
+        """ 
+        Are the self variables (units, natom, masses, charges, geom) from the xyz file initialized correctly?
+        """
         
         with open('input1.xyz', 'r') as file1:
             mol1 = Molecule(file1.read())
@@ -22,10 +26,10 @@ class Test(unittest.TestCase):
                          'checking geometry')
 
     def test_copy_function(self):
-        """ Does the copy function correctly initialize all variables of a new molecule?
-            
-            Also, is the new molecule truly a different object? (ie: changing properties of
-            the original does not affect the copy and vice versa)? """
+        """ 
+        Does the copy function correctly initialize all variables of a new molecule? Also, is the new molecule
+        truly a different object? (ie: changing properties of the original does not affect the copy and vice versa)?
+        """
                 
         with open('input1.xyz', 'r') as file1:
             mol1 = Molecule(file1.read())
@@ -42,8 +46,9 @@ class Test(unittest.TestCase):
         self.assertNotEqual(mol1.geom, mol2.geom)
 
     def test_unit_conversion_accuracy(self):
-        """ 1.0 Angstrom is approximately 1.889725989 Bohr. Is this conversion (and its reverse) 
-            carried out correctly? """
+        """ 
+        1.0 Angstrom is approximately 1.889725989 Bohr. Is this conversion (and its reverse) carried out correctly?
+        """
 
         with open('input1.xyz', 'r') as file1:
             mol1 = Molecule(file1.read())
@@ -55,8 +60,10 @@ class Test(unittest.TestCase):
             self.assertAlmostEqual(mol1.geom[i][2] * 1.889725989, mol2.geom[i][2])
 
     def test_unit_conversion_symmetry(self):
-        """ Does converting back and forth between bohrs and angstroms introduce and compound rounding errors?
-            Each operation should exactly reverse its counterpart. """
+        """ 
+        Does converting back and forth between bohrs and angstroms introduce and compound rounding errors? Each 
+        operation should exactly reverse its counterpart.
+        """
                 
         with open('input1.xyz', 'r') as file1:
             mol1 = Molecule(file1.read())
@@ -66,6 +73,21 @@ class Test(unittest.TestCase):
             mol2.to_angstrom()
         self.assertEqual(mol1.geom, mol2.geom)
 
+    def test_iteration(self):
+        """ 
+        Can the atoms of the molecule be iterated over? Each atom should be a tuple containing the atom's label 
+        and a numpy array of the 3 cartesian coordinates.
+        """
+
+        with open('input1.xyz', 'r') as file1:
+            mol1 = Molecule(file1.read())
+        
+        newlabels = []
+        for atom in mol1:
+            newlabels += atom[0]
+
+        self.assertEqual(mol1.labels, newlabels, 'checking labels')
+        #TODO: assert that the coordinates are also correct
 
 if __name__ == '__main__':
     unittest.main()
