55062e565faf02594d38a84c73edbb68f7d1ff6b
zachglick <glickzachary@gmail.com>
2017-06-14 09:38:30 -0400

    Basics of Molecule implemented. Two tests have been writted

--- 0/zachglick/molecule.py
+++ 0/zachglick/molecule.py
@@ -1,36 +1,77 @@
+import masses as m
+import copy
+
 class Molecule :
 
     # Default Methods
 
-    def __init__(self) :
-        pass
+    def __init__(self, data) :
 
+        data = data.split('\n')
+        self.natom = int(data[0])
+        self.units = data[1]
+        
+        self.labels = []
+        self.masses = []
+        self.charges = []
+        self.geom = []
+        
+        for atom in data[2:] :
+            
+            atom = atom.split()
+            
+            if len(atom) != 4 :
+                continue # Throw Error?
+            self.labels.append(atom[0])
+            self.masses.append(m.get_mass(atom[0]))
+            self.charges.append(m.get_charge(atom[0]))
+            self.geom.append(atom[1:])
+        
+        self.geom = [[float(coord) for coord in atom] for atom in self.geom]
+        
     def to_bohr(self) :
-        pass
+        if self.units == 'Angstrom' :
+            self.units = 'Bohr'
+            self.geom =[[coord * 1.889725989 for coord in atom] for atom in self.geom]
 
     def to_angstrom(self) :
-        pass
+        if self.units == 'Bohr' :
+            self.units = 'Angstrom'
+            self.geom =[[coord / 1.889725989 for coord in atom] for atom in self.geom]
 
     def xyz_string(self) :
-        pass
-
+        ans = "Molecule: " + " ".join(self.labels)
+        return ans
+    
     def copy(self) :
-        pass
+        return copy.deepcopy(self)
 
     # Advanced Methods
 
     def __len__(self) :
-        pass
+        return self.natom
 
     def __str__(self) :
-        pass
-
+        ans = "Molecule: " + " ".join(self.labels)
+        return ans
+    
     def __repr__(self) :
-        pass
+        return "\n".join(self.labels)
 
     def __iter__(self) :
         pass
 
     def __add__(self, mol) :
-        pass
+        if self.units == 'Angstroms' :
+            mol.to_bohr()
+        else :
+            mol.to_angstrom()
+        newmol = self.copy()
+        newmol.natom += mol.natom
+        newmol.geom += mol.geom
+        newmol.labels += mol.labels
+        newmol.masses += mol.masses
+        return newmol
 
+if __name__ == '__main__':
+    print("This is a supporting class and not meant to be run on its own")
--- 0/zachglick/test.py
+++ 0/zachglick/test.py
@@ -1,13 +1,48 @@
 import unittest
-from molecule import Molecule as mol
-print("hey")
+from molecule import Molecule
 
 class Test(unittest.TestCase):
-    """Tests for `primes.py`."""
+    """ Tests for Molecule class in 'molecule.py' """
     
-    def test_something_important(self):
-        """Is five successfully determined to be prime?"""
-        mol1 = mol.Molecule("todo: things")
+    def test_self_variables(self):
+        """ Are the initial self variables (units, natom, masses, charges, geom)
+            initialized correctly? """
+        
+        with open('input1.xyz', 'r') as file1:
+            mol1 = Molecule(file1.read())
+        self.assertEqual(mol1.units, 'Angstrom', 'checking units')
+        self.assertEqual(mol1.natom, 3, 'checking natom')
+        self.assertEqual(mol1.labels, ['O', 'H', 'H'], 'checking labels')
+        self.assertEqual(mol1.masses, [15.99491461956, 1.00782503207, 1.00782503207], 'checking masses')
+        self.assertEqual(mol1.charges, [8, 1, 1], 'checking charges')
+        self.assertEqual(mol1.geom,
+                         [[0.0, 0.0, -0.0711762954],
+                          [0.0, -0.8916195680, 0.5648097613],
+                          [0.0, 0.8916195680, 0.5648097613]],
+                         'checking geometry')
+
+    def test_copy_function(self):
+        """ Does the copy function initialize all variables of the new molecule?
+            
+            Also, is the new molecule truly a different object? (ie: changing properties of
+            the original does not affect the copy)"""
+                
+        with open('input1.xyz', 'r') as file1:
+            mol1 = Molecule(file1.read())
+        mol2 = mol1.copy()
+        self.assertEqual(mol1.units, mol2.units, 'checking units')
+        self.assertEqual(mol1.natom, mol2.natom, 'checking natom')
+        self.assertEqual(mol1.labels, mol2.labels, 'checking labels')
+        self.assertEqual(mol1.masses, mol2.masses, 'checking masses')
+        self.assertEqual(mol1.charges, mol2.charges, 'checking charges')
+        self.assertEqual(mol1.geom, mol2.geom, 'checking geometry')
+
+        mol2.to_bohr()
+        self.assertNotEqual(mol1.units, mol2.units)
+        self.assertNotEqual(mol1.geom, mol2.geom)
+
+
+
 
 if __name__ == '__main__':
     unittest.main()
