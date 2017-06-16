a14fb705ed91e8e35b24cb4377e21e4cbb0e89ba
mbowman <mbowman205@gmail.com>
2017-06-16 16:41:52 -0400

    Added some magic methods

--- 0/mbowman/molecule.py
+++ 0/mbowman/molecule.py
@@ -15,15 +15,15 @@ class Molecule(object):
 		:param mole_str: string (typically .xyz file) describing a molecule
 		:param units: units for geometry, angstrom or bohr (default is angstrom) 	
 		"""	
-		self.parse(mole_str)
+		self.parse_molecule(mole_str)
 
-	def parse(self, mole_str):
+	def parse_molecule(self, mole_str):
 		"""
 		Parses data from .xyz file to change class attributes
 		:param mole_str: string (typically .xyz file) describing a molecule
 		"""
 		lines = mole_str.strip().split("\n")
-		self.natom = lines[0]
+		self.natom = int(lines[0])
 		if lines[1].lower() == "angstrom":
 			self.units = "angstrom"
 		elif lines[1].lower() == "bohr":
@@ -71,10 +71,52 @@ class Molecule(object):
 		"""
 		Returns copy of self
 		"""
-		return self
+		tempMol = Molecule(self.xyz_string())
+		return tempMol
+	
+	def __len__(self):
+		"""
+		Overrides len(str) to instead return the number of atoms
+		"""
+		return self.natom
+
+	def __str__(self):
+		"""
+		Overrides str() to instead return the Molecule object in a pretty format
+		"""
+		return "yes"
+
+	def __print__(self):
+		"""
+		Overrides print() to print the result of __str__ 
+		"""
+		print str()
+
+	def __repr__(self):
+		"""
+		Overrides repr() to reurn .xyz format instead
+		"""
+		return xyz_string()
+	
+	def __add__(mol1, mol2):
+		"""
+		Overrides addition, combining two molecule objects to create a new molecule object
+		The units for the new molecule object are equivalent to the first molecule
+		This method does not currently check for redundancies
+		:param mol1: First molecule instance, units taken from here
+		:param mol2: Second molecule instance, units must be converted before addition
+		"""
+		tempMol = mol1.copy()
+		tempMol.natom += mol2.natom
+		tempMol.masses += mol2.masses
+		tempMol.charges += mol2.charges
+
+		return tempMol
 
 #Used to test Molecule class
 if __name__ == "__main__":
 	mole_str = open("../../extra-files/molecule.xyz").read()
 	mol = Molecule(mole_str)
-	print mol.xyz_string()
+	mol2 = mol.copy()
+	mol3 = mol + mol2
+	print len(mol3)	
