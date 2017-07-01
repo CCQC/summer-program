9f72c38c5513b13d4e5422a3bb8084e19bdf43ef
mbowman <mbowman205@gmail.com>
2017-07-01 11:46:22 -0400

    Bug fixed by transposing e-e repulsion matrix from chemist to physicist notation

--- 3/mbowman/rhf.py
+++ 3/mbowman/rhf.py
@@ -5,51 +5,60 @@ import math
 
 class RHF(object):
 
-	def __init__(self, mol, mints, convCrit = 10, maxIter = 500):
+	def __init__(self, mol, mints):
 		"""
-		:param mol: 
-		:param mints:
-		:param convCrit: criteria for converge, x corresponds to maximum difference of 10^-x
+		Initializes molecule, molecular properties, molecular integrals,
+		before entering a loop to calculate the RHF energy 
+
+		:param mol: molecule object, specifies geometry, charge, and multiplicity 
+		:param mints: molecular integral helper, generates various molecular intergrals
+		:param convCrit: criteria for converge, x in input corresponds to maximum difference of 10^-x
+		:param maxIter: maximum number of iterations to obtain self consistence
 		"""
 			
 		self.mol = mol
 		self.mints = mints
-		self.convCrit = convCrit
-		self.maxIter = maxIter
-		self.E = 0.0
+		self.convCrit = psi4.core.get_global_option('E_CONVERGENCE')
+		self.maxIter = psi4.core.get_global_option('MAXITER')
 		
 		#Step 1: Read nuclear repulsion energy from molecule and atomic integrals from MintsHelper	
-		self.VNuc = mol.nuclear_repulsion_energy() #nuclear repulsion energy
-		self.S = np.array(mints.ao_overlap()) #overlap integrals
-		self.T = np.array(mints.ao_kinetic()) #kinetic energy integrals
-		self.V = np.array(mints.ao_potential()) #electron-nuclear attraction integrals
-		self.g = np.array(mints.ao_eri()) #electron-electron repulsion integrals
-		self.norb = mints.basisset().nbf() #number of orbits (defines size of arrays) 	
-		self.H = self.T + self.V #Hamiltonian
-		self.nelec = - mol.molecular_charge()
+		self.VNuc = mol.nuclear_repulsion_energy() 		#nuclear repulsion energy
+		self.S = np.array(mints.ao_overlap()) 			#overlap integrals
+		self.T = np.array(mints.ao_kinetic()) 			#kinetic energy integrals
+		self.V = np.array(mints.ao_potential()) 		#electron-nuclear attraction integrals
+		self.g = np.array(mints.ao_eri()).transpose(0,2,1,3) 	#electron-electron repulsion integrals, transposed from (pq|rs) to <pr|qs>
+		#The transpose is very important, because from here on forward physicist notation is assumed!
+		
+		#Step 1.5: Calculate Hamiltonian and orbital information 
+		self.H = self.T + self.V 		#Hamiltonian
+		self.E = 0.0 				#RHF energy
+		self.norb = mints.basisset().nbf() 	#number of orbits (defines size of arrays) 	
+		self.nelec = - mol.molecular_charge() 	#number of electrons
 		for atom in range(mol.natom()):
 			self.nelec += mol.Z(atom)
-		self.nocc = int(self.nelec / 2)
+		self.nocc = int(self.nelec / 2)		#number of occupied orbitals
 					
 		#Step 2: Form orthogonalizer (X = S^-1/2)
-		self.X = spla.inv(spla.sqrtm(self.S))
-		
+		self.X = np.matrix(spla.inv(spla.sqrtm(self.S)))	#Orthogonalizer S^-1/2
 		
 		#Step 3: Set D = 0 as "core" guess
-		self.D = np.zeros((self.norb,self.norb))
+		self.D = np.zeros((self.norb,self.norb))		#Density Matrix
 		
 		#Iteration to SC
-		convCond = False #convergence condition initially set to false once the energy and density matrix converge it
-		self.Eold = 1.0
-		self.Dold = np.zeros((self.norb,self.norb))  
-		self.iter = 0
+		convCond = False 				#convergence condition 
+		self.Eold = 1.0					#Previous calculated energy
+		self.Dold = np.zeros((self.norb,self.norb))  	#Previous density matrix
+		self.iter = 0					#Iteration count
+		
 		line = "+----+--------------------+------------+"
 		print line
 		print  "|iter| Energy             | dE         |"
 		print line
 		while not convCond:
-			self.I2SC()	
-			if (np.absolute((self.Eold - self.E)) < 10**(-self.convCrit)  and self.iter > 1):
+			self.I2SC() #Steps 1-7
+			#Check convergence, if the energy and density matrix are both within a threshold of one another, then it has converged
+			#Additionally the program must iterate twice to avoid the condition when all four variables are initially null
+			if (np.absolute((self.Eold - self.E)) < self.convCrit and np.absolute(spla.norm(self.D) - spla.norm(self.Dold)) < self.convCrit and self.iter > 1):
 				print line
 				print  "| Converged                            |"
 				convCond = True
@@ -64,42 +73,33 @@ class RHF(object):
 		"""
 		Iteration to(2) Self Consistency, this function is called iteratively until the energy converges
 		"""
-		pass
+		#Step 0: set the old values for E and D to the result of the previous iteration
 		self.Eold = self.E
 		self.Dold = self.D
 
-		#Step 1: Build Fock matrix (ie "giving a fock")
-		self.J = np.einsum('prqs,sr->pq', self.g, self.D) #Sum of Columb Operators
-		# print self.J
-		self.K = np.einsum('prsq,sr->pq', self.g, self.D) #Sum of Exchange Operators
-		# print self.K
-		self.F = self.H + self.J - 0.5 *self.K	#Fock Operator
-		# print self.F	
+		#Step 1: Build Fock matrix 
+		self.J = np.einsum('prqs,sr->pq', self.g, self.D)	#Sum of Columb Operators
+		self.K = np.einsum('prsq,sr->pq', self.g, self.D) 	#Sum of Exchange Operators
+		self.F = self.H + self.J - 0.5 *self.K			#Fock Operator
 		
 		#Step 2: Transform Fock to orthogonalized AO basis
 		self.FOrtho = np.einsum('ik,kj->ij', (np.einsum('ik,kj->ij',self.X,self.F)) , self.X) #Orthogonalized Fock matrix (XFX)
-		# print self.FOrtho	
 		
 		#Step 3: Diagonilze Orthonalized Fock 
 		self.orbitalEnergies, self.COrtho = np.linalg.eigh(self.FOrtho) #Orbital Energies and MO coefficients
-		# print self.COrtho
 		
 		#Step 4: Backtransform MO Coefficients
-		self.C = np.einsum('ik,kj->ij',self.X,self.COrtho) #Original basis
-		#print self.C
+		self.C = np.einsum('ik,kj->ij',self.X,self.COrtho) 	#Original basis
 		
 		#Step 5: Rebuild density matrix
-		self.Cocp = self.C[:,:self.nocc]
-		self.D = 2*np.einsum('pi,qi->pq',self.Cocp,np.conj(self.Cocp)) #New density matrix 
-		#print self.D
+		self.Cocp = self.C[:,:self.nocc]				#Truncated basis to only include occupied orbitals
+		self.D = 2*np.einsum('pi,qi->pq',self.Cocp,np.conj(self.Cocp)) 	#New density matrix 
 		
-		#Step 6: Calculate energy (For convenience steps 5 and 6 have been rearranged wrt the inst
-                sum = self.H + 0.5* self.J+ 0.25*self.K #sum of Hamiltonian and Fock matrices
-                self.E = np.einsum('pq,qp',sum,self.D) + self.VNuc #New energy, congratulations
+		#Step 6: Calculate energy 
+                sum = self.H + 0.5* self.J+ 0.25*self.K 		#sum of H + 1/2 v
+                self.E = np.einsum('pq,qp',sum,self.D) + self.VNuc 	#New energy, congratulations
 	
 		#Step 7: Print and increase iteration count
 		print '|{0:4d}| {1:16.14f} | {2:4.4e} |'.format(self.iter, self.E,np.absolute(self.E - self.Eold))
 		self.iter +=1	
-		#print "Energy: " + str(self.E)
-		#print "Iteration: " + str(self.iter)	
 
