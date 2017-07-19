cbe43a8cf456178efe61a32985a0977a57aa0cd9
mdav2 <matthew.davis010@gmail.com>
2017-07-18 23:38:01 -0700

    Finished project 1

--- 1/mdav2/frequencies.py
+++ 1/mdav2/frequencies.py
@@ -2,6 +2,11 @@ import numpy
 import geom
 import math
 
+ccms = 3E10 #speed of light in centimeters per second
+eh = 4.36E-18 #J/hartree
+ao = 5.29E-11 #m/br
+u = 1.66E-27 #kg/amu
+
 def readhessian(filename):
     """opens and reads a hessian matrix from the given filename"""
     hessianfile = open(filename, 'r')
@@ -32,7 +37,49 @@ def frequencies(molecule,hessian):
     for vector in range(0,molecule.natom * 3):
         unweighted[vector] = evecs[vector]/math.sqrt(molecule.masses[math.floor(vector/3.)])
     reorg = unweighted.T
-    return reorg, evals
+    evals = evals * (eh/(u * ao **2))
+    frequencies = []
+    for idx,frequency in enumerate(evals):
+        freq = ( (1./(math.pi * 2) * numpy.sqrt(frequency + 0J))/ccms) #+0J to force complex evaluation
+        #turns imaginary frequencies to negative
+        if freq.imag != 0:
+            freq = -1*freq.imag
 
-        
+        frequencies.append(freq)
+    sortingdictionary = dict(zip(frequencies,reorg))
+    modes = []
+    freqs = []
+    #sorts to ensure high > low order of frequencies
+    for key in sorted(sortingdictionary)[::-1]:
+        modes.append(sortingdictionary[key])
+        freqs.append(key)
+
+
+    return modes, freqs
+
     
+def printfrequencies(molecule, frequencies, normalmodes):
+    bigstring = ""
+    for idx,frequency in enumerate(frequencies):
+        outstring = ""
+        outstring += str(molecule.natom) + "\n"
+        
+        outstring += "%4.2f" % frequency.real +" cm^-1" + "\n"
+        i = 0
+        for atom in range(0, molecule.natom):
+            outstring += '{: 012.11f}'.format(molecule.geom[atom][0]) + "   "
+            outstring += '{: 012.11f}'.format(molecule.geom[atom][1]) + "   "
+            outstring += '{: 012.11f}'.format(molecule.geom[atom][2]) + "   "
+            outstring += '{: 012.11f}'.format(normalmodes[idx][i])    + "   "
+            outstring += '{: 012.11f}'.format(normalmodes[idx][i+1])  + "   "
+            outstring += '{: 012.11f}'.format(normalmodes[idx][i+2])  + "   "
+
+            outstring += '\n'
+            
+            i += 3
+            i = i % 8
+        
+        bigstring += outstring + '\n'
+    f = open('example.xyz','w')
+    f.write(bigstring)
+    f.close()
--- 1/mdav2/testfreq.py
+++ 1/mdav2/testfreq.py
@@ -5,5 +5,9 @@ hessian = frequencies.readhessian('hessian.dat')
 molecule = geom.Molecule('molecule.xyz')
 
 vecs, vals = frequencies.frequencies(molecule,hessian)
+"""
 for idx, val in enumerate(vals):
      print(val, vecs[idx])
+"""
+
+frequencies.printfrequencies(molecule,vals,vecs)
