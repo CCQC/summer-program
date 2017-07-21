fb7dbb63086addd54fec392b59fc211d43c042ff
mdav2 <matthew.davis010@gmail.com>
2017-07-21 11:19:02 -0700

    Incorporated edits on project 0

--- 0/mdav2/molecule.py
+++ 0/mdav2/molecule.py
@@ -9,15 +9,15 @@ class Molecule:
     filename as input.
     """
     def __init__(self,data):
-        #checking if input string is filename or xyzstring
-        if len(data.split('\n')) != 1:
+        #checking if input string is filename or xyzstring by determining number of lines
+        if len(data.split('\n')) >= 1:
             self.moleculelines = data.split('\n')
         else:
             with open(data, 'r') as moleculefile:
                 self.moleculelines = moleculefile.readlines()
 
         #units are supplied on line 2
-        self.units = str(self.moleculelines[1]).rstrip()
+        self.units = self.moleculelines[1].rstrip()
         #number of atoms is supplied on line 1
         self.natom = int((self.moleculelines[0]).rstrip())
 
@@ -32,13 +32,11 @@ class Molecule:
             #+2 because atoms start at line 3
             element,x,y,z = self.moleculelines[atom+2].split()
 
-            self.labels[atom] = str(element)
-            self.charges[atom] = int(masses.get_charge(self.labels[atom]))
-            self.masses[atom] = float(masses.get_mass(self.labels[atom]))
+            self.labels[atom] = element
+            self.charges[atom] = masses.get_charge(self.labels[atom])
+            self.masses[atom] = masses.get_mass(self.labels[atom])
+            self.geom[atom] = [x,y,z]
 
-            for i in range(0, 3):
-                #-1 for indexing reasons
-                self.geom[atom][i] = [x,y,z][i]
     def to_bohr(self):
         #converts all units to Bohr radii when called
         #1 br = 0.52917724888
@@ -53,16 +51,12 @@ class Molecule:
             self.units = "Angstrom"
             self.geom *= (1./1.889725989)
     def xyz_string(self):
-        #returns a string
-        outstring = ""
-        outstring += str(self.natom) + "\n"
-        outstring += str(self.units) + "\n"
+        #returns a string containing the object in .xyz format.
+        outstring = '{}\n{}\n'.format(*[self.natom,self.units])
+        
         for atom in range(0, self.natom):
-            outstring += str(self.labels[atom]) + "     "
-            outstring += '{: 012.11f}'.format(self.geom[atom][0]) + "   "
-            outstring += '{: 012.11f}'.format(self.geom[atom][1]) + "   "
-            outstring += '{: 012.11f}'.format(self.geom[atom][2]) + "   "
-            outstring += "\n"
+            outlist = [self.labels[atom]] + self.geom[atom].tolist()
+            outstring += ('{:6}' + '{: 012.11f}   '*3+'\n').format(*outlist)
         return outstring
     def copy(self):
         return Molecule(self.xyz_string())
