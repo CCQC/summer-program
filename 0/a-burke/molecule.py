c9c90988b383b15d74aa7616ace5ec84c4c4aad7
Ally Burke <ab99072@vlogin2.ccqc.uga.edu>
2018-06-14 10:06:50 -0400

    Updated

--- 0/a-burke/molecule.py
+++ 0/a-burke/molecule.py
@@ -12,42 +12,35 @@ class Molecule(object):
 
 	#Used to initialize an instance of the Molecule class
 
-	def __init__(self, geom_str, units = "Angstroms"):
+	def __init__(self, geom_str, units):
 		self.units = units				#Specifies units for spatial coordinates
-		self.read(geom_str)				#Reads in the geometry file for the specific molecule	
-		self.natom = self.read(geom_str)		#Specifies the number of atoms in the molecule
-		self.labels = self.read(geom_str)		#Specifies list of atom symbols following the input file	
-		
-		#unsure how to reference parameters without hardcoding
-		self.masses = get_mass("H") 	        	#Reads in masses from input file
-		self.charges = get_charge("H")			#Reads in charges from input file
-
-	#Unsure if read function is necessary for this project
-
-	def read(self, geom_str):
-		
-		#Read in the file containing the geometry of the molecule in question
-	
-        	geom = []
-        	self.label = []
-        	lines = geom_str.strip().splitlines()
-        	natom = int(lines[0])
-        	for line in lines[2:]:
-           		atom, x, y, z = line.split() 
-           		self.label.append(atom)
-           		geom.append([float(x), float(y), float(z)])
-        	self.geom = np.array(geom)
+		self.read(geom_str)				#Reads in the geometry file for the specific molecule		
 
 	#Returns a string representing the contents of the Molecule object in .xyz format
-
-	def xyz_string(self):
-		string = "{}\n{}\n".format(self.natom, self.units)
+	
+	def read(self, geom_str):
+		#Read in the file containing the geometry of the molecule in question
+		geom = []
+		self.labels = []
+		self.masses = []
+		self.charges = []
 		lines = geom_str.strip().splitlines()
-		for line in lines:
-			atom, x, y, z = line.split()
-			string.append(atom)
-			string.append("   " + x + "   " + y + "   " + z)
-		return string
+		self.natom = int(lines[0])
+		for line in lines[2:]:
+			atom, x, y, z = line.split() 
+			self.labels.append(atom) 
+			geom.append([float(x), float(y), float(z)])
+		self.geom = np.array(geom)
+		for x in self.labels:
+			self.masses.append(get_mass(x))
+			self.charges.append(get_charge(x))
+		y = 0
+		for x in self.labels:
+			print("Mass of " + x + " is: " + str(self.masses[y]))
+			print("Charge of " + x + " is: " + str(self.charges[y]))
+			y += 1
+#get_mass and get_charge functions not being used (should append into self.masses and self.charges
+
 					
 
 	#UNIT CONVERSION FUNCTIONS
@@ -55,31 +48,35 @@ class Molecule(object):
 	def to_bohr(self):
 
 	#Converts units from Angstrom to Bohr	
-
 		if self.units == "Angstrom":
 			self.units = "Bohr"
-			self.units *= 1.889725989
-		elif self.units == "Bohr":
-			print("Units already in Bohr")
-
+			self.geom *= 1.889725989
+			
 	def to_angstrom(self):
 		
 	#Converts from Bohr to Angstrom
 
 		if self.units == "Bohr":
 			self.units = "Angstrom"
-			self.units /= 1.889725989
-		elif self.units == "Angstrom":
-			print("Units already in Angstrom")
+			self.geom /= 1.889725989
 
 	#Create fresh copy of Molecule object
 	
 	def copy(self):
-		x = new_geom_str.copy(geom_str)
+		x = Molecule(self.read(geom_str))
 		return x			
 		
 	
 if __name__ == "__main__":	
 	file1 = open("../../extra-files/molecule.xyz").read()
-	mol = Molecule(file1)
-	print(mol)
+	mol = Molecule(file1, "Angstrom")
+	print("Units are currently in: " + mol.units)
+	print(mol.geom)
+	print("Number of atoms: " + str(mol.natom))
+	print("Atoms: " + str(mol.labels))
+	mol.to_bohr()
+	print("Units have now been converted to: " + mol.units)
+	print(mol.geom)
+	mol.to_angstrom()
+	print("Units have now been converted back to: " + mol.units)
+	print(mol.geom)
